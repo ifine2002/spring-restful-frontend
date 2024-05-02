f339b1876b50ee34687154cc274ccc8300e98323
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-05-02 16:46:26 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app';
-import { Provider } from 'react-redux'
-import { store } from '@/redux/store';
+import { callFetchAccount } from './config/api';
+// import App from './app';
+// import { Provider } from 'react-redux'
+// import { store } from '@/redux/store';
 
+const App1 = () => {
+  const getAccount = async () => {
+    const res = await callFetchAccount();
+    if (res && res.data) {
+      // dispatch(doGetAccountAction(res.data))
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, [])
+
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Hello CORS with HoiDanIT</h1>
+    </div>
+  )
+}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    {/* <App /> */}
+    {/* </Provider> */}
+    <App1 />
   </React.StrictMode>,
 )
