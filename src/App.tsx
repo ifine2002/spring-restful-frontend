baf44185a2bbb7f460c5689da5d573ffda178b4f
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-05-02 16:32:59 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import {
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { callFetchAccount } from 'config/api';
 import NotFound from 'components/share/not.found';
-import Loading from 'components/share/loading';
-import LoginPage from 'pages/auth/login';
-import RegisterPage from 'pages/auth/register';
-import LayoutAdmin from 'components/admin/layout.admin';
-import ProtectedRoute from 'components/share/protected-route.ts';
+// import Loading from 'components/share/loading';
+// import LoginPage from 'pages/auth/login';
+// import RegisterPage from 'pages/auth/register';
+// import LayoutAdmin from 'components/admin/layout.admin';
+// import ProtectedRoute from 'components/share/protected-route.ts';
 import Header from 'components/client/header.client';
 import Footer from 'components/client/footer.client';
 import HomePage from 'pages/home';
 import 'styles/app.module.scss';
-import DashboardPage from './pages/admin/dashboard';
-import CompanyPage from './pages/admin/company';
-import PermissionPage from './pages/admin/permission';
-import ResumePage from './pages/admin/resume';
-import RolePage from './pages/admin/role';
-import UserPage from './pages/admin/user';
+// import DashboardPage from './pages/admin/dashboard';
+// import CompanyPage from './pages/admin/company';
+// import PermissionPage from './pages/admin/permission';
+// import ResumePage from './pages/admin/resume';
+// import RolePage from './pages/admin/role';
+// import UserPage from './pages/admin/user';
 
 const LayoutClient = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -68,66 +68,66 @@ export default function App() {
       ],
     },
 
-    {
-      path: "/admin",
-      element: <LayoutAdmin />,
-      errorElement: <NotFound />,
-      children: [
-        {
-          index: true, element:
-            <ProtectedRoute>
-              <DashboardPage />
-            </ProtectedRoute>
-        },
-        {
-          path: "company",
-          element:
-            <ProtectedRoute>
-              <CompanyPage />
-            </ProtectedRoute>
-        },
-        {
-          path: "user",
-          element:
-            <ProtectedRoute>
-              <UserPage />
-            </ProtectedRoute>
-        },
+    // {
+    //   path: "/admin",
+    //   element: <LayoutAdmin />,
+    //   errorElement: <NotFound />,
+    //   children: [
+    //     {
+    //       index: true, element:
+    //         <ProtectedRoute>
+    //           <DashboardPage />
+    //         </ProtectedRoute>
+    //     },
+    //     {
+    //       path: "company",
+    //       element:
+    //         <ProtectedRoute>
+    //           <CompanyPage />
+    //         </ProtectedRoute>
+    //     },
+    //     {
+    //       path: "user",
+    //       element:
+    //         <ProtectedRoute>
+    //           <UserPage />
+    //         </ProtectedRoute>
+    //     },
 
-        {
-          path: "resume",
-          element:
-            <ProtectedRoute>
-              <ResumePage />
-            </ProtectedRoute>
-        },
-        {
-          path: "permission",
-          element:
-            <ProtectedRoute>
-              <PermissionPage />
-            </ProtectedRoute>
-        },
-        {
-          path: "role",
-          element:
-            <ProtectedRoute>
-              <RolePage />
-            </ProtectedRoute>
-        }
-      ],
-    },
+    //     {
+    //       path: "resume",
+    //       element:
+    //         <ProtectedRoute>
+    //           <ResumePage />
+    //         </ProtectedRoute>
+    //     },
+    //     {
+    //       path: "permission",
+    //       element:
+    //         <ProtectedRoute>
+    //           <PermissionPage />
+    //         </ProtectedRoute>
+    //     },
+    //     {
+    //       path: "role",
+    //       element:
+    //         <ProtectedRoute>
+    //           <RolePage />
+    //         </ProtectedRoute>
+    //     }
+    //   ],
+    // },
 
 
-    {
-      path: "/login",
-      element: <LoginPage />,
-    },
+    // {
+    //   path: "/login",
+    //   element: <LoginPage />,
+    // },
 
-    {
-      path: "/register",
-      element: <RegisterPage />,
-    },
+    // {
+    //   path: "/register",
+    //   element: <RegisterPage />,
+    // },
   ]);
 
   return (
