b9f46df4abca2c6f1f8aa9b6b0881074301e3e1d
ifine2002 <boyyhp1@gmail.com>
2024-12-08 04:32:34 +0700

    init version 2

--- vite.config.ts
+++ vite.config.ts
@@ -11,25 +11,25 @@ dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 // https://v2.vitejs.dev/config/#environment-variables
 export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [
-      react(), 
-      // visualizer() as PluginOption
-    ],
-    server: {
-      port: parseInt(env.PORT)
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src/"),
-        components: `${path.resolve(__dirname, "./src/components/")}`,
-        styles: `${path.resolve(__dirname, "./src/styles/")}`,
-        config: `${path.resolve(__dirname, "./src/config/")}`,
-        pages: `${path.resolve(__dirname, "./src/pages/")}`,
-      },
-    },
-  }
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [
+            react(),
+            // visualizer() as PluginOption
+        ],
+        server: {
+            port: parseInt(env.PORT)
+        },
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src/"),
+                components: `${path.resolve(__dirname, "./src/components/")}`,
+                styles: `${path.resolve(__dirname, "./src/styles/")}`,
+                config: `${path.resolve(__dirname, "./src/config/")}`,
+                pages: `${path.resolve(__dirname, "./src/pages/")}`,
+            },
+        },
+    }
 })
